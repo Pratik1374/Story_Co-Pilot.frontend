da43f17873c25ef22d307d40cf9ecbf05a44189e
Pratik <pratik1374@gmail.com>
2024-03-31 14:44:24 +0530

    modified login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,6 +9,7 @@ import { GoogleAuthProvider, signInWithPopup } from "@firebase/auth";
 import { getAuth } from "@firebase/auth";
 import { lobster } from "@/utils/fonts";
 import Image from "next/image";
+import toast from "react-hot-toast";
 
 const Login: NextPage = () => {
   const [email, setEmail] = useState<string>("");
@@ -19,13 +20,21 @@ const Login: NextPage = () => {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("got")
+    console.log("got");
 
     try {
       await login(email, password);
       await router.push("/");
     } catch (error) {
       console.log(error);
+      toast.error("Please check your credentials", {
+        style: {
+          fontWeight: "bold",
+          border: "3px solid red",
+          borderRadius: "50px",
+          backgroundColor: "white",
+        },
+      });
     }
   };
 
@@ -36,6 +45,14 @@ const Login: NextPage = () => {
       await router.push("/");
     } catch (error: any) {
       console.error("Google sign-in error:", error);
+      toast.error("Something went wrong", {
+        style: {
+          fontWeight: "bold",
+          border: "3px solid red",
+          borderRadius: "50px",
+          backgroundColor: "white",
+        },
+      });
     }
   };
 
@@ -200,11 +217,11 @@ const Login: NextPage = () => {
     //   </div>
 
     <div className="h-screen w-screen flex flex-col">
-      <div className="flex items-center justify-center w-full h-full">
+      <div className="flex lg:items-center justify-center w-full h-full">
         <div className="flex w-full lg:w-1/2 p-8 items-center justify-center">
           <div className="h-full">
-          <h1 className="lg:hidden relative text-3xl font-semibold font-serif before:absolute before:inset-0 before:animate-typewriter before:bg-black">
-              Welcome to<span> </span>
+            <h1 className="lg:hidden text-3xl font-semibold font-serif animate-appearance-in mb-5 mt-2">
+              Welcome to<span><br/> </span>
               <Link
                 href="/"
                 className={`${lobster.className} story-pilot-link text-3xl font-bold cursor-pointer text-center`}
@@ -212,76 +229,81 @@ const Login: NextPage = () => {
                 Story_Co-Pilot
               </Link>
             </h1>
-            <h2 className="text-xl font-semibold mb-4 lg:pr-24">
-              Login With Your Account
-            </h2>
-            <form onSubmit={handleSubmit}>
-              <div className="mb-4">
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium text-gray-600"
-                >
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  placeholder="Email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  className="w-full mt-3 p-2 rounded-md border-t-1 border-cyan-200 text-cyan-300 bg-black shadow-sm shadow-cyan-200"
-                  required
-                />
-              </div>
-              <div className="mb-4">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium text-gray-500"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  id="password"
-                  value={password}
-                  placeholder="Password"
-                  onChange={(e) => setPassword(e.target.value)}
-                  className="w-full mt-3 p-2 rounded-md border-t-1 border-cyan-200 text-cyan-300 bg-black shadow-sm shadow-cyan-200"
-                  required
-                />
-              </div>
-             
-              <div className="flex w-full">
+            <div className="flex flex-col p-6 bg-black py-8 rounded-lg login-card-shadow">
+              <h2 className="text-xl font-semibold mb-4 lg:pr-24">
+                Login With Your Account
+              </h2>
+              <form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium text-gray-600"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="w-full mt-3 p-2 rounded-md border-t-1 border-gray-600 text-cyan-300 bg-black shadow-sm shadow-cyan-200"
+                    required
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium text-gray-500"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="w-full mt-3 p-2 rounded-md border-t-1 border-gray-600 text-cyan-300 bg-black shadow-sm shadow-cyan-200"
+                    required
+                  />
+                </div>
+
+                <div className="flex w-full">
+                  <button
+                    type="submit"
+                    className={`w-full relative inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-gray-900 rounded-full group bg-gradient-to-br from-purple-500 to-pink-700 group-hover:from-purple-500 group-hover:to-purple-500-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 mt-5 ${
+                      loggingIn ? "cursor-not-allowed " : ""
+                    }`}
+                    disabled={loggingIn}
+                  >
+                    <p className="flex w-full justify-center items-center px-5 py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-950 rounded-full group-hover:bg-opacity-0 group-hover:text-black text-center font-sans font-semibold">
+                      Login
+                    </p>
+                  </button>
+                </div>
+              </form>
               <button
-              type="submit"
-                className={`w-full relative inline-flex items-center justify-center p-0.5 mb-2 overflow-hidden text-gray-900 rounded-full group bg-gradient-to-br from-purple-500 to-pink-700 group-hover:from-purple-500 group-hover:to-purple-500-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 mt-5 ${
-                  loggingIn ? "cursor-not-allowed " : ""
-                }`}
-                disabled={loggingIn}
+                className="mt-3 w-full flex rounded-full items-center border border-sky-300  pl-2 p-2 gap-1  hover:border-0 hover:shadow-sm hover:shadow-sky-300"
+                onClick={handleGoogleSignIn}
               >
-                <p className="flex w-full justify-center items-center px-5 py-2 transition-all ease-in duration-75 bg-white dark:bg-gray-950 rounded-full group-hover:bg-opacity-0 group-hover:text-black text-center font-sans font-semibold">
-                  Login
-                </p>
+                <Image
+                  src="/google_icon.svg"
+                  alt="google_icon"
+                  width={30}
+                  height={30}
+                />
+                <p className="cursor-pointer">Continue with Google</p>
               </button>
+              <div className="flex items-center mt-2 justify-center">
+              <Link href="/register" className="hover:border-b-1 border-blue-700 text-gray-300">Don't have an account? Sign Up here</Link>
               </div>
-            </form>
-            <button
-              className="mt-3 w-full flex rounded-full items-center border border-sky-300  pl-2 p-2 gap-1  hover:border-0 hover:shadow-sm hover:shadow-sky-300"
-              onClick={handleGoogleSignIn}
-            >
-              <Image
-                src="/google_icon.svg"
-                alt="google_icon"
-                width={30}
-                height={30}
-              />
-              <p className="cursor-pointer">Continue with Google</p>
-            </button>
+            </div>
           </div>
         </div>
-        <div className="hidden lg:flex lg:h-full">
-          <div className="flex flex-col  items-center justify-center">
-            <h1 className="relative text-3xl font-semibold font-sans before:absolute before:inset-0 before:animate-typewriter before:bg-black bg-black p-3">
+        <div className="hidden lg:flex lg:h-full overflow-hidden">
+          <div className="flex flex-col items-center justify-center">
+            <h1 className="relative text-3xl font-semibold font-sans before:absolute before:inset-0 before:animate-typewriter before:bg-black bg-black p-1">
               Welcome to<span> </span>
               <Link
                 href="/"
@@ -290,11 +312,11 @@ const Login: NextPage = () => {
                 Story_Co-Pilot
               </Link>
             </h1>
-            <div className="animate-appearance-in">
+            <div className="animate-appearance-in -mt-4">
               <img
                 src="/AI_bot.svg"
                 alt="logo"
-                className="w-[450px] lg:h-[250px] h-[550px] lg:mt-0 mt-[-100px]"
+                className="w-[450px] lg:h-[250px] h-[550px]"
               />
             </div>
           </div>
