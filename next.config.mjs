49c2060a55d38bc7241a66876253d9aa72a22ff0
Pratik <pratik1374@gmail.com>
2024-04-10 09:33:32 +0530

    handled login with google error

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,24 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-        port: '',
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        port: "",
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin allow-popups",
+          },
+        ],
+      },
+    ];
   },
 };
 
